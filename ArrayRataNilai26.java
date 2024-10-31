77d19caa09dcfb638b2a9506528bf32fb6084eca
Sweper27 <rifqiariessaputra@gmail.com>
2024-10-31 11:51:42 +0700

    mengubah syntax untuk menjawab pertanyaan 1-2

--- ArrayRataNilai26.java
+++ ArrayRataNilai26.java
@@ -4,17 +4,31 @@ public class ArrayRataNilai26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nilaiMhs= new int [10];
-        double total = 0, rata2;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMahasiswa = sc.nextInt();
 
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        int[] nilaiMhs= new int [jumlahMahasiswa];
+        double totalLulus = 0, rata2, totalTidaklulus = 0;
+        int countLulus = 0, countTidakLulus = 0;
+
+        for (int i = 0; i < jumlahMahasiswa; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
             nilaiMhs[i] = sc.nextInt();
         }
-        for (int i = 0; i < nilaiMhs.length; i++){
-            total += nilaiMhs [i];
+        for (int i = 0; i < jumlahMahasiswa; i++){
+            if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
+                countLulus++;
+            } else {
+                totalTidaklulus += nilaiMhs[i];
+                countTidakLulus++;
+            }
         }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai ="+rata2);
+        double rata2Lulus = (countLulus > 0) ? totalLulus / countLulus : 0;
+        double rata2TidakLulus = (countTidakLulus > 0) ? totalTidaklulus / countTidakLulus : 0;
+
+        System.out.println("\nRata-rata nilai lulus = " + rata2Lulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rata2TidakLulus);
+
     }
 }
